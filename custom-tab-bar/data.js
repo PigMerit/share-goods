b548f35b09db31c8975c4a0de68619055123e9b1
liuzhenpeng <liuzhenpeng@kuaishou.com>
2022-08-18 09:36:18 +0800

    feat(): 项目初始化

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'src/pages/index/index',
+  },
+  {
+    icon: 'user',
+    text: '个人主页',
+    url: 'src/pages/home/home',
+  },
+];
